056a5037baf9206d21c3a264acf78d1b6b05a259
“Tserfaty” <tss9970@nyu.edu>
2023-03-20 19:46:47 +0000

    Delete item in shopcart

--- service/routes.py
+++ service/routes.py
@@ -260,6 +260,27 @@ def update_items(shopcart_id, item_id):
     return make_response(jsonify(item.serialize()), status.HTTP_200_OK)
 
 
+######################################################################
+# DELETE AN ITEM
+######################################################################
+@app.route("/shopcarts/<int:shopcart_id>/items/<int:item_id>", methods=["DELETE"])
+def delete_items(shopcart_id, item_id):
+    """
+    Delete an Item
+
+    This endpoint will delete an Item based the id specified in the path
+    """
+    app.logger.info(
+        "Request to delete Item %s for Shopcart id: %s", (item_id, shopcart_id)
+    )
+
+    # See if the item exists and delete it if it does
+    item = Item.find(item_id)
+    if item:
+        item.delete()
+
+    return make_response("", status.HTTP_204_NO_CONTENT)
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
